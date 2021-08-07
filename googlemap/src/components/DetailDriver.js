4e56811327a6d886470e24a55eede9154c59ad53
sepsep <safemontima@gmail.com>
2021-08-07 15:00:43 +0700

    simple w8 and driver-detail

--- googlemap/src/components/DetailDriver.js
+++ googlemap/src/components/DetailDriver.js
@@ -4,7 +4,10 @@ export default function DetailDriver(props){
     const { cancelQueue } = props; 
     return(
         <div className="detail-driver-container">
-            <button className="cancel-button" onClick={cancelQueue}>ยกเลิก</button>
+            <h4>กรุณารอ2</h4>
+            <div id="driver-detail"></div>
+            <div id="money-detail"></div>
+            <button className="cancel-button" type="button" type="button" class="btn btn-primary" id="buttcancel" onClick={cancelQueue}>ยกเลิก</button>
         </div>
     )
 }
--- googlemap/src/components/Wait.js
+++ googlemap/src/components/Wait.js
@@ -4,9 +4,10 @@ export default function Wait(props){
     const { cancelQueue } = props; 
     
     return(
-        <div className="waiting-box">
-            <h4> กรุณารอ</h4>
-            <button className="cancel-button" onClick={cancelQueue}>ยกเลิก</button>
+        <div className="waiting-box" >
+            <h4>กรุณารอ1</h4>
+            <div id="w8detail"></div>
+            <button className="cancel-button" type="button" type="button" class="btn btn-primary" id="buttcancel" onClick={cancelQueue}>ยกเลิก</button>
         </div>
     )
 }
