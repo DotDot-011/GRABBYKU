a60977bd091899c3b07d1a7cc6ddb7ef921e8c7b
sepsep <safemontima@gmail.com>
2021-08-08 03:28:27 +0700

    add detail waiting and driver

--- googlemap/src/components/Wait.js
+++ googlemap/src/components/Wait.js
@@ -5,8 +5,13 @@ export default function Wait(props){
     
     return(
         <div className="waiting-box" >
-            <h4>กรุณารอ1</h4>
-            <div id="w8detail"></div>
+            <h4>กรุณารอ</h4>
+            <h5>'-เวลาโดยประมาณ-'</h5>
+            <div id="waiting-detail">
+                <div class="num-queue">จำนวนคิวที่รอ : $</div>
+                <div class="num-driver">จำนวนผู้ให้บริการ : $</div>
+                <div class="money-est">จำนวนเงินที่ต้องชำระ : $</div>
+            </div>
             <button className="cancel-button" type="button" type="button" class="btn btn-primary" id="buttcancel" onClick={cancelQueue}>ยกเลิก</button>
         </div>
     )
