e19d537750910fc2ccdcd2bcc15a048aee1090ba
sepsep <safemontima@gmail.com>
2021-08-08 04:13:01 +0700

    user : fix image and driver detail

--- googlemap/src/components/DetailDriver.js
+++ googlemap/src/components/DetailDriver.js
@@ -6,16 +6,14 @@ export default function DetailDriver(props){
         <div className="detail-driver-container">
             <h4>กรุณารอ</h4>
             <h5>'-เวลาโดยประมาณ-'</h5>
-            <div class="driver-detail">
+            <div class="driver-detail" >
+                <div class="image-driver"></div>
                 <div class="name-driver">นายธนาคาร  หลักแหลม</div>
                 <div class="name-license">ทะเบียนรถ : กอจ 666</div>
                 <div class="name-number">เลขประจำตัว : 47</div>
                 <div class="name-position">ประจำจุด : ประตูนรก</div>
             </div>
-            <div class="money-div">
-                {/* <div class="money-est">จำนวนเงินที่ต้องชำระ : $</div> */}
-            จำนวนเงินที่ต้องชำระ : $
-            </div>
+            <div class="money-div">จำนวนเงินที่ต้องชำระ : $</div>
             <button className="cancel-button" type="button" type="button" class="btn btn-primary" id="buttcancel" onClick={cancelQueue}>ยกเลิก</button>
         </div>
     )
