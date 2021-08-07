c02631519e74a5019d3dae1c17e97839f2ab50d4
inwten <inwten2549@gmail.com>
2021-08-07 18:08:33 +0700

    add userdriverinbook

---
+++ backend/homedriver/checkuser/checkuser.php
@@ -0,0 +1,26 @@
+<?php
+
+$postData = json_decode(file_get_contents("php://input"));
+
+$driver_id = $postData->driver_id;
+
+// file_put_contents("./registerUser/test.txt", $postData);
+
+$sql = "SELECT user_id FROM booking WHERE driver_id = '$driver_id'";
+$result = $conn->query($sql);
+$data = [];
+
+if ($result->num_rows > 0) {
+    $i = 0;
+    while ($row = $result->fetch_assoc()) {
+
+        $data[$i]["user_id"] = $row['user_id'];
+        $i++;
+    }
+    echo json_encode($data, JSON_PRETTY_PRINT);
+} else {
+    echo "0 results";
+}
+
+
+$conn->close();
