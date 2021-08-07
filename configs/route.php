70d6a8d631246ff6f2bb46cc79947548e69b096f
inwten <inwten2549@gmail.com>
2021-08-07 15:24:27 +0700

    test

--- configs/route.php
+++ configs/route.php
@@ -1,6 +1,7 @@
 <?php
 
-// $routes["/backend/api/data"]["GET"] = "./data/test.php";
+$routes["/backend/api/data"]["POST"] = "./data/test.php";
+$routes["/backend/api/test_get"]["GET"] = "./data/testGet.php";
 // $routes["/backend/api/testpost"]["POST"] = "./data/testPost.php";
 
 $routes["/backend/api/register_user"]["POST"] = "./registerUser/register_user.php";
