ce63d93724680c2da476ffdc6b6a18bac0f16cff
jukkraputp <jukkraput.p@ku.th>
2021-08-07 16:13:08 +0700

    git commit -m

--- backend/configs/route.php
+++ backend/configs/route.php
@@ -4,8 +4,8 @@ $routes["/backend/api/data"]["POST"] = "./data/test.php";
 $routes["/backend/api/test_get"]["GET"] = "./data/testGet.php";
 // $routes["/backend/api/testpost"]["POST"] = "./data/testPost.php";
 
-$routes["/backend/api/register_user"]["POST"] = "./registerUser/register_user.php";
-$routes["/backend/api/register_driver"]["POST"] = "./registerDriver/register_driver.php";
+$routes["/backend/api/register_user"]["POST"] = "./register/user/register_user.php";
+$routes["/backend/api/register_driver"]["POST"] = "./register/user/register_driver.php";
 
 $routes["/backend/api/login_user"]["POST"] = "./login/user/login.php";
 $routes["/backend/api/login_driver"]["POST"] = "./login/driver/login.php";
