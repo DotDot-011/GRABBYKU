bb66a0c51e1a98a7de32cd0368330f0ab834bc83
watermelon03 <weerakarn.s@ku.th>
2021-09-06 18:38:07 +0700

    fix layout in login-regis

--- googlemap/src/LinkToBackend.js
+++ googlemap/src/LinkToBackend.js
@@ -1,1 +1,1 @@
-export const Url = { LinkToBackend: "http://1a62-2001-fb1-54-c607-3851-b720-e82-bd79.ap.ngrok.io/"};
+export const Url = { LinkToBackend: "http://df50-2001-fb1-54-c607-956a-6ead-ab5e-7e70.ap.ngrok.io/"};
