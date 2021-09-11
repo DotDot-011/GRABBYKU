a263aeb6437ceb41d922a4c198bed4b69f7a4628
Kx-N <65390633+Kx-N@users.noreply.github.com>
2021-09-11 21:22:38 +0700

    add work finish WebSocket
    สำคัญมาก!!"อย่าลืมไปเพิ่มroute ใน config ด้วย push ไฟล์ configให้ไม่ได้"

--- src/WorkFinished.php
+++ src/WorkFinished.php
@@ -1,11 +1,59 @@
 <?php
 
-$driver_name = $wsdata['arg1'];
-$driver_id = $wsdata['arg2'];
-$user_name = $wsdata['arg3'];
-$user_id = $wsdata['arg4'];
-$driver_name_and_id = $driver_name . $driver_id;
-$user_name_and_id = $user_id . $user_id;
+$driver_name = $wsdata['driver_name'];
+$driver_id = $wsdata['driver_id'];
+$user_name = $wsdata['user_name'];
+$user_id = $wsdata['user_id'];
+//$cost = $wsdata['cost'];
+$driver_name_and_id = $driver_name." ".$driver_id;
+$user_name_and_id = $user_name." ".$user_id;
 
+
+
+//Set on service in websocket of driver to 0
 $sql = "UPDATE websocket SET on_service = 0 WHERE name = '$driver_name_and_id' or name = '$user_name_and_id'";
 $conn->query($sql);
+
+
+//notify user arrive
+$sql2 = "SELECT * FROM websocket WHERE name LIKE '$user_name_and_id' ";
+$result_for_trigger = $conn->query($sql2);
+if($result_for_trigger != NULL){
+ $row=$result_for_trigger->fetch_assoc();
+ foreach($this->clients as $client){
+  if($client->resourceId == $row['connection_id']){
+   print("answer to socket: ".$row['connection_id']."\n");
+   $client->send(json_encode([
+                 "message_code" => "arrive"
+   ]));
+  }
+ }
+}
+
+//Delete booking when done
+$sql3 = "DELETE FROM booking WHERE driver_id = '$driver_id' ";
+$conn->query($sql3);
+
+
+
+//notify driver "booking has been delete"
+$sql4 = "SELECT * FROM websocket WHERE name LIKE '$driver_name_and_id' ";
+print($driver_name_and_id); 
+$result_for_answer = $conn->query($sql4);
+if($result_for_answer != NULL){
+ print(" try answer \n");
+ $row2=$result_for_answer->fetch_assoc();
+ foreach($this->clients as $client){
+  if($client->resourceId == $row2['connection_id']){
+   $client->send(json_encode([
+    "message_code" => "หิวหมัด"
+   ]));
+  }
+ }
+}
+
+
+//update success_count
+$sql5 = "UPDATE user Set success_count = success_count + 1 WHERE user_id = '$user_id' ";
+$conn->query($sql5);
+
