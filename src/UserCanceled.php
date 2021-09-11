60c271a225095ddba81bebcefc346bb964524944
inwten <inwten2549@gmail.com>
2021-09-11 15:16:11 +0700

    edit usercalcel

--- src/UserCanceled.php
+++ src/UserCanceled.php
@@ -4,13 +4,15 @@ $user_name = $wsdata['arg1'];
 $user_id = $wsdata['arg2'];
 
 $sql = "SELECT driver_id FROM booking WHERE user_id = '$user_id' and driver_id IS NOT NULL";
+$result=$conn->query($sql);
+
 if ($result->num_rows == 1) {
     $row = $result->fetch_assoc();
     $driver_id = $row['driver_id'];
     $sql = "SELECT fname, lname FROM driver WHERE driver_id = '$driver_id'";
     $result = $conn->query($sql);
     $row = $result->fetch_assoc();
-    $driver_name_and_id = $row['fname'] . " " . $row['lname'] . $driver_id;
+    $driver_name_and_id = $row['fname'] . " " . $row['lname'] . " " . $driver_id;
     $sql = "SELECT connection_id FROM websocket WHERE name = '$driver_name_and_id'";
     $result = $conn->query($sql);
     $row = $result->fetch_assoc();
