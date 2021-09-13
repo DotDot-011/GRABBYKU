2493311fcffd5b20a6649ccd1956cc7a6f294368
jukkraputp <jukkraput.p@ku.th>
2021-09-13 16:42:11 +0700

    add image to database

--- backend/register/driver/register.php
+++ backend/register/driver/register.php
@@ -13,10 +13,11 @@ $driver_no = $postData->driver_no;
 $win_name = $postData->win_name; 
 $username = $postData->username; 
 $password = $postData->password;
+$imageData = $postData->image;
 // file_put_contents("./registerUser/test.txt", $postData);
 
-$sql = "INSERT INTO driver (fname, lname, birth_date, age, plate, phone, id_no, driver_no, win_name, username, password)
-VALUES ('$fname','$lname','$birth_date','$age','$plate','$phone','$id_no','$driver_no','$win_name', '$username','$password')";
+$sql = "INSERT INTO driver (fname, lname, birth_date, age, plate, phone, id_no, driver_no, win_name, username, password, imageData)
+VALUES ('$fname','$lname','$birth_date','$age','$plate','$phone','$id_no','$driver_no','$win_name', '$username','$password', '$imageData')";
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
