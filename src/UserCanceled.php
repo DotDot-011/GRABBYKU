9f48cf01e9cb2dc8ef43db3d40d991d5b7bf90c2
wipp <wippstkk@gmail.com>
2021-09-15 20:36:12 +0700

    udate query

--- src/UserCanceled.php
+++ src/UserCanceled.php
@@ -4,16 +4,14 @@ $user_name = $wsdata['arg1'];
 $user_id = $wsdata['arg2'];
 
 $sql = "SELECT driver_id FROM booking WHERE user_id = '$user_id' and driver_id IS NOT NULL";
-$result=$conn->query($sql);
-
 if ($result->num_rows == 1) {
     $row = $result->fetch_assoc();
     $driver_id = $row['driver_id'];
     $sql = "SELECT fname, lname FROM driver WHERE driver_id = '$driver_id'";
     $result = $conn->query($sql);
     $row = $result->fetch_assoc();
-    $driver_name_and_id = $row['fname'] . " " . $row['lname'] . " " . $driver_id;
-    $sql = "SELECT connection_id FROM websocket WHERE name = '$driver_name_and_id'";
+    $driver_name_and_id = $row['fname'] . " " . $row['lname'] . $driver_id;
+    $sql = "SELECT connection_id FROM websocket WHERE name = '$name' and id = '$driver_id' and is_driver = 1";
     $result = $conn->query($sql);
     $row = $result->fetch_assoc();
     $connection_id = $row['connection_id'];
@@ -21,6 +19,7 @@ if ($result->num_rows == 1) {
         if ($from->resourceId == $connection_id) {
             $client->send(json_encode([
                 "message_code"=> $protocol,
+                "message"=> "Canceled"
             ]));
         }
     }
@@ -29,7 +28,7 @@ if ($result->num_rows == 1) {
 $statement = "DELETE FROM booking WHERE user_id = '$user_id' ";
 
 if ($conn->query($statement) === TRUE) {
-    echo "delete laew ja =] ";
+    echo "deleted ";
 } else {
     echo "Error: " . $conn->error;
 }
