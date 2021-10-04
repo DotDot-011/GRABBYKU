f54bfb8e629af3017623be6cd18f6c1555408164
sepsep <safemontima@gmail.com>
2021-10-04 09:49:15 +0700

    add username/img defalse/text logo lowprofile

--- googlemap/src/LinkToBackend.js
+++ googlemap/src/LinkToBackend.js
@@ -1,9 +1,9 @@
 
 export const Url = { 
-    LinkToBackend: "https://c156-2001-fb1-54-a340-70ec-ce7a-8154-5deb.ap.ngrok.io/",
+    LinkToBackend: "http://ed23-49-237-13-132.jp.ngrok.io/",
 };
 
 export const socketUrl = { 
-    LinkToWebSocket: "wss://a72e-2001-fb1-54-a340-70ec-ce7a-8154-5deb.ap.ngrok.io",
+    LinkToWebSocket: "ws://75ae-49-237-13-132.ap.ngrok.io",
 };
 
