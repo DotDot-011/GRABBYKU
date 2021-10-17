932b270ae24a333e209c742af7e1193743ddde62
watermelon03 <weerakarn.s@ku.th>
2021-10-17 17:55:07 +0700

    fix button hover and fix abit

--- googlemap/src/LinkToBackend.js
+++ googlemap/src/LinkToBackend.js
@@ -1,9 +1,9 @@
 
 export const Url = { 
-    LinkToBackend: "http://4e7b-223-24-153-160.jp.ngrok.io/",
+    LinkToBackend: "https://f98f-27-55-90-220.jp.ngrok.io/",
 };
 
 export const socketUrl = { 
-    LinkToWebSocket: "ws://fffa-223-24-153-160.ap.ngrok.io",
+    LinkToWebSocket: "wss://818a-27-55-90-220.ap.ngrok.io",
 };
 
