71c6049622af75c1f6ef74954b2566fa13685394
sepsep <safemontima@gmail.com>
2021-10-18 10:02:15 +0700

    add overflow in some heigh | sticky logo | set padding 0 for IOS |

--- googlemap/src/LinkToBackend.js
+++ googlemap/src/LinkToBackend.js
@@ -1,9 +1,9 @@
 
 export const Url = { 
-    LinkToBackend: "https://f98f-27-55-90-220.jp.ngrok.io/",
+    LinkToBackend: "http://f98f-27-55-90-220.jp.ngrok.io/",
 };
 
 export const socketUrl = { 
-    LinkToWebSocket: "wss://818a-27-55-90-220.ap.ngrok.io",
+    LinkToWebSocket: "ws://818a-27-55-90-220.ap.ngrok.io",
 };
 
