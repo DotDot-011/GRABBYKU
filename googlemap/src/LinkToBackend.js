2082276ae49192f3085e21f6f07ce456f6cfdc46
PhyXerSs <kuntaice28@gmail.com>
2021-10-18 11:49:03 +0700

    register user bug fix

--- googlemap/src/LinkToBackend.js
+++ googlemap/src/LinkToBackend.js
@@ -1,9 +1,9 @@
 
 export const Url = { 
-    LinkToBackend: "http://f98f-27-55-90-220.jp.ngrok.io/",
+    LinkToBackend: "https://8806-2001-fb1-54-a6a-35e0-9bec-6dd5-b95b.ap.ngrok.io/",
 };
 
 export const socketUrl = { 
-    LinkToWebSocket: "ws://818a-27-55-90-220.ap.ngrok.io",
+    LinkToWebSocket: "wss://4c42-2001-fb1-54-a6a-35e0-9bec-6dd5-b95b.ap.ngrok.io",
 };
 
--- googlemap/src/component_login/regis_user.js
+++ googlemap/src/component_login/regis_user.js
@@ -91,7 +91,7 @@ function RegisUser() {
         (confirmPasswordRef.current.value != '') ? Count++ : NotificationManager.warning('กรุณากรอกยืนยันรหัสผ่าน');
         
         if(passwordRef.current.value === confirmPasswordRef.current.value & Count===10 & validateEmail(emailRef.current.value) 
-        & passwordRef.current.value.length >= 4 & citizenIdRef.current.value.length ==13) {
+        & passwordRef.current.value.length >= 4 & citizenIdRef.current.value.length ==13 && 2021 - birth_dateRef.current.value.split('-')[0] >=18) {
             axios.post(Url.LinkToBackend +"backend/api/register_user",{
                 fname: fnameRef.current.value,
                 lname: lnameRef.current.value,
@@ -143,6 +143,9 @@ function RegisUser() {
             }
             if(phoneRef.current.value.length != 10){
                 NotificationManager.warning('หมายเลขโทรศัพท์ไม่ถูกต้อง');   
+            }
+            if(2021 - birth_dateRef.current.value.split('-')[0] <18){
+                NotificationManager.warning('อายุยังไม่ถึงเกณฑ์');
             }
         }
         
